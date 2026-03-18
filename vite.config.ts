c2b8f37a54990efadfe9a5ab5d2aa6b9f61c1fdb
mynolog <minholee.log@gmail.com>
2026-03-18 16:41:38 +0900

    fix: Vite 설정 내 Node 내장 모듈 임포트 방식 최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import * as path from 'node:path'
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
